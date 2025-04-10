27ef624fa43be488b0d9bbe9ef31276bc9f2a7ef
YejiJeonn <yejijeon02@gmail.com>
2025-04-10 22:27:19 +0900

    Initial commit

---
+++ src/main/java/com/backend/itcuniv/ItcUnivApplication.java
@@ -0,0 +1,13 @@
+package com.backend.itcuniv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItcUnivApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItcUnivApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/backend/itcuniv/ItcUnivApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.itcuniv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItcUnivApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
