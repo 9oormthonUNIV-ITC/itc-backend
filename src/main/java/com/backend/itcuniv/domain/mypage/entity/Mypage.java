9227e294b10c0cd0c4e0c201cdf2c5a71a72dbc9
YejiJeonn <yejijeon02@gmail.com>
2025-05-14 16:34:22 +0900

    :sparkles: feat: Pause myPage

---
+++ src/main/java/com/backend/itcuniv/domain/mypage/entity/Mypage.java
@@ -0,0 +1,14 @@
+//package com.backend.itcuniv.domain.mypage.entity;
+//
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Table;
+//import lombok.Getter;
+//import lombok.NoArgsConstructor;
+//
+//@Entity
+//@Table(name = "user")
+//@Getter
+//public class Mypage {
+//    private Long id;
+//    private
+//}
