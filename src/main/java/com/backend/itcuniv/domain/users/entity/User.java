e513a76ef981cbda5941d948800bdaf7a983bb2d
YejiJeonn <yejijeon02@gmail.com>
2025-05-19 20:05:54 +0900

    :recycle: refactor: Change User structure for mypage(func. change userName)

--- src/main/java/com/backend/itcuniv/domain/users/entity/User.java
+++ src/main/java/com/backend/itcuniv/domain/users/entity/User.java
@@ -25,6 +25,8 @@ public class User {
     private String givenName; // 이름
     @Column(name = "family_name")
     private String familyName; // 성
+    @Column(name = "user_name")
+    private String userName; // 사용자 이름
     @Column(name = "picture")
     private String picture; // 프로필 사진
     @Column(name = "admin_token")
@@ -37,6 +39,7 @@ public class User {
         this.name = name;
         this.givenName = givenName;
         this.familyName = familyName;
+        this.userName = familyName + givenName;
         this.picture = picture;
         this.adminToken = 0;
     }
