68290964978225d9bc58d43c103e9e842f21dd20
YejiJeonn <yejijeon02@gmail.com>
2025-05-19 20:51:10 +0900

    :sparkles: feat: Add mypage func

--- src/main/java/com/backend/itcuniv/domain/users/repository/UserRepository.java
+++ src/main/java/com/backend/itcuniv/domain/users/repository/UserRepository.java
@@ -11,12 +11,14 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Boolean existsByGoogleId(String googleId);
 
-    Optional<User> findIdByNickname(String nickname);
-
     @Query("SELECT u.id FROM User u WHERE u.googleId = :googleId")
     Long findIdByGoogleId(@Param("googleId") String googleId);
 
     @Query("SELECT u.adminToken FROM User u WHERE u.googleId = :googleId")
     Integer findAdminTokenByGoogleId(@Param("googleId") String googleId);
 
+
+    String finduserNameByGoogleId(String googleId);
+
+    String finduserEmailByGoogleId(String googleId);
 }
