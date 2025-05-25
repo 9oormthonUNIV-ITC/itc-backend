50ab87f23bfc6413c1ff0744fcac4cc805e19c9f
YejiJeonn <yejijeon02@gmail.com>
2025-05-26 01:45:56 +0900

    :bug: fix: Modify mypageService to fit userRepository

--- src/main/java/com/backend/itcuniv/domain/mypage/service/MypageService.java
+++ src/main/java/com/backend/itcuniv/domain/mypage/service/MypageService.java
@@ -13,16 +13,16 @@ public class MypageService {
     private final UserRepository userRepository;
 
     public MypageResponseDto getMyInfo(String adminToken) {
-        String userName = userRepository.findUserNameByGoogleId(adminToken);
-        String email = userRepository.findEmailByGoogleId(adminToken);
+        String userName = userRepository.findNicknameByGoogleId(adminToken).getFirst().getNickname();
+        String email = userRepository.findEmailByGoogleId(adminToken).getFirst().getEmail();
 
         return new MypageResponseDto(userName, email);
     }
 
     @Transactional
     public MypageResponseDto updateMyInfo(String adminToken) {
-        String userName = userRepository.findUserNameByGoogleId(adminToken);
-        String email = userRepository.findEmailByGoogleId(adminToken);
+        String userName = userRepository.findNicknameByGoogleId(adminToken).getFirst().getNickname();
+        String email = userRepository.findEmailByGoogleId(adminToken).getFirst().getEmail();
 
         MypageResponseDto mypageResponseDto = new MypageResponseDto(userName, email);
         mypageResponseDto.update(userName);
